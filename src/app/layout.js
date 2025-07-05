470bb47d72b7f43f172bbeb8bb59e9e132a2d1eb
Minu248 <wkdalsdn5656@gmail.com>
2025-07-05 16:28:23 +0900

    feat: 오픈그래프 수정 2차

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,7 @@ export const metadata = {
     type: "website",
     images: [
       {
-        url: "https://onesongindie.vercel.app/og-image.png",
+        url: "https://onesongindie.com/og-image.png",
         width: 885,
         height: 460,
         alt: "들어볼래? 한 곡 Indie 오픈그래프 이미지"
